f1b9ee93b4a1f15cd0a0cff65a816c2bdcecd717
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-08-17 07:43:32 -0400

    Removed placeholder routes. Uncommented BlogsController routes.

--- routes/web.php
+++ routes/web.php
@@ -32,26 +32,7 @@ Auth::routes();
 
 
 
-/****** DELETE ONCE CONTROLLER IS COMPLETED ******/
-/* START BLOG ROUTES */
-Route::get('blogs', function () {
-    return view('pages.blogs');
-})->name('blogs');
 
-Route::get('blogs/create', function () {
-    return view('pages.create-blog');
-})->name('create-blog');
-
-Route::post('blogs/create', 'BlogsController@store')->name('store-blog');
-
-Route::get('blogs/{id}', function () {
-    return view('pages.show-blog');
-})->name('show-blog');
-
-Route::get('blogs/{id}/edit', function () {
-    return view('pages.edit-blog');
-})->name('update-blog');
-/* END BLOG ROUTES */
 
 
 
@@ -59,13 +40,12 @@ Route::get('blogs/{id}/edit', function () {
 
 /****** UNCOMMENT ONCE CONTROLLER IS COMPLETED ******/
 ///* Routes for blogs CRUD */
-//Route::prefix('blogs')->group(function () {
-//    Route::get('/', 'BlogsController@index')->name('blogs');
-//
-//    Route::get('/create', 'BlogsController@create')->name('create-blog');
-//    Route::post('/create', 'BlogsController@store')->name('store-blog');
-//    Route::get('/{id}', 'BlogsController@show')->name('show-blog');
-//    Route::get('/{id}/edit', 'BlogsController@edit')->name('edit-blog');
-//    Route::patch('/{id}', 'BlogsController@update')->name('update-blog');
-//    Route::delete('/{id}', 'BlogsController@delete')->name('delete-blog');
-//});
+Route::prefix('blogs')->group(function () {
+  Route::get('/', 'BlogsController@index')->name('blogs');
+  Route::get('/create', 'BlogsController@create')->name('create-blog');
+  Route::post('/create', 'BlogsController@store')->name('store-blog');
+  Route::get('/{id}', 'BlogsController@show')->name('show-blog');
+  Route::get('/{id}/edit', 'BlogsController@edit')->name('edit-blog');
+  Route::patch('/{id}', 'BlogsController@update')->name('update-blog');
+  Route::delete('/{id}', 'BlogsController@delete')->name('delete-blog');
+});
